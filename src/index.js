b71d59c07333f6d896979060993bde82e7d449f2
CodenificienT <codenificient@tutanota.com>
2021-12-31 18:51:02 -0500

    remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import App from './App'
+import './index.css'
+
+// STORE - GLOBALIZED STATE
+
+// ACTION - INCREMENT
+const increment = () => {
+	return {
+		type: 'INCREMENT'
+	}
+}
+// DECREMENT
+const decrement = () => {
+	return {
+		type: 'DECREMENT'
+	}
+}
+
+// REDUCER
+const counter = (state = 0, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1
+
+		case 'DECREMENT':
+			return state - 1
+	}
+}
+
+let store = createStore(counter)
+// DISPLAY IN THE CONSOLE
+store.subscribe(() => console.log(store.getState()))
+
+// DISPATCH
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(decrement())
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+)
